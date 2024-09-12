193752ce8d2640ced2b618d0b4ff39dbaf0b962c
Christian Clauss <cclauss@me.com>
2024-09-12 12:24:16 +0200

    Define EducationResource and PedigreeRecord

---
+++ src/education_resource.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class EducationResource(BaseModel):
+    """
+    The metadata for an education resource to be communicated in the Exchange.  This
+    could be a lesson plan, one or more datasets, documents, worksheets, images, or
+    videos.
+    The resource contains a title, description, author, authoring institution, a list
+    of subject tags, a creation date, last modified date, and usage information.
+    """
+
+    from .pedigree_record import PedigreeRecord
+
+    title: str = ""
+    description: str = ""
+    author: str = ""
+    authoring_institution: str = ""
+    subject_tags: list[str] = []
+    creation_date: datetime | None = None
+    last_modified_date: datetime | None = None
+    usage: list[PedigreeRecord] = []
