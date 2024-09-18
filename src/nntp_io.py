6a803d1b00fcabdd15f0870118aa5f44d5e7dec0
Christian Clauss <cclauss@me.com>
2024-09-18 16:41:18 +0200

    nntp_write and nntp_read

--- src/nntp_io.py
+++ src/nntp_io.py
@@ -16,31 +16,29 @@ Message-ID: <test.inews.{}@inn2.packages.debian.org>
 """
 
 
-def nntp_write(payload: dict, newsgroup: str = "local.test") -> int:
+def nntp_write(payload: dict, newsgroup: str = "local.test") -> str:
     """
     Write a payload dict as json to a InterNet News newsgroup.
+
+    Return: test.inews.a253222105b64597b9afd10e4f4c6740@inn2.packages.debian.org
     """
     msg = FMT.format(newsgroup, uuid.uuid4().hex, json.dumps(payload, indent=2))
     # print(f"{msg = }")
     with nntplib.NNTP("localhost", readermode=True) as nntp_server:
-        _response = nntp_server.post(msg.encode("utf-8"))
-        print(f"{_response = }")
-        print(f"{nntp_server.stat()}")
-        return nntp_server.stat()[2]
-        # Verify that the article is there.
-        _resp, _count, _first, last, _name = nntp_server.group(newsgroup)
-        return last
+        response = nntp_server.post(msg.encode("utf-8")).split()
+        assert response.startswith("240 "), f"{response = }"
+        return response.split()[-1][1:-1]  # strip off the <>
 
 
-def nntp_read(article_id: int, newsgroup: str = "local.test") -> dict:
+def nntp_read(article_id: str, newsgroup: str = "local.test") -> dict:
     """
     Read an article from a InterNet News newsgroup and return it as a dict.
     """
     with nntplib.NNTP("localhost", readermode=True) as nntp_server:
-        _resp, _count, _first, _last, _name = nntp_server.group(newsgroup)
-        _stat = nntp_server.stat(article_id)
-        _article = nntp_server.article(article_id)
-        return json.loads(_article[1].lines[0].decode("utf-8"))
+        article = nntp_server.article(article_id)
+        print(f"{article = }")
+        print(f"{article.decode("utf-8") = }")
+        return {}  # json.loads(article[1].lines[0].decode("utf-8"))
 
 
 if __name__ == "__main__":
