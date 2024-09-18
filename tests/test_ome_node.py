3849053f243f9319a54a400875e187773ce869a2
Christian Clauss <cclauss@me.com>
2024-09-18 22:07:07 +0200

    pytest tests/test_ome_node.py

--- tests/test_ome_node.py
+++ tests/test_ome_node.py
@@ -1,6 +1,15 @@
 import nntplib
+from typing import Iterator
 
 from server import ome_node
+
+
+def get_newsgroups() -> Iterator[str]:
+    client = ome_node.getClient()
+    response, newsgroups = client.list()
+    assert response.startswith("215")
+    for newsgroup in newsgroups:
+        yield newsgroup.group
 
 
 def test_get_client():
@@ -9,7 +18,7 @@ def test_get_client():
     assert client.nntp_version == 2
     assert client.nntp_implementation == "INN 2.6.4"
     assert client.getwelcome().split()[0] == "200"
-    newsgroups = [newsgroup.group for newsgroup in client.list()[1]]
+    newsgroups = list(get_newsgroups())
     for newsgroup in ome_node.DEFAULT_NEWSGROUPS:
         assert newsgroup in newsgroups
 
@@ -17,3 +26,15 @@ def test_get_client():
 def test_channels():
     channels = list(ome_node.channels())
     assert not channels
+
+
+def test_channel_summary():
+    client = ome_node.getClient()
+    for newsgroup in get_newsgroups():
+        response, count, first, last, name = client.group(newsgroup)
+        assert response.startswith("211")
+        channel_summary = ome_node.channelSummary(newsgroup)
+        assert channel_summary.name == name == newsgroup
+        assert channel_summary.estimatedTotalArticles == count
+        assert channel_summary.firstArticle == first
+        assert channel_summary.lastArticle == last
