c14e5fb5aeed215ed7c8eb4e837bd7619f35f451
Christian Clauss <cclauss@me.com>
2024-09-18 21:04:49 +0200

    pytest tests/test_ome_node.py

--- tests/test_ome_node.py
+++ tests/test_ome_node.py
@@ -3,7 +3,7 @@ from server import ome_node
 
 def test_get_client():
     client = ome_node.getClient()
-    assert isinstance(client, ome_node.nntp.NNTPClient)
+    assert isinstance(client, ome_node.nntp.NNTP)
     assert client.host == "localhost"
     assert client.port == 119
     assert client.use_ssl is False
