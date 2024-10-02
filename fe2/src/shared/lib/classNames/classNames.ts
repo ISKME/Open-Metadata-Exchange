7a84d0e7cb4a28c145dcbcc3eeed9288b19fe634
Anoop Aryal <anooparyal@sr71>
2024-10-02 10:33:00 -0500

    adding another set of .gitignored files

---
+++ fe2/src/shared/lib/classNames/classNames.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
+  return [
+    cls,
+    ...additional.filter(Boolean),
+    ...Object.entries(mods)
+      .filter(([_, value]) => Boolean(value))
+      .map(([className]) => className),
+  ]
+    .join(' ');
+}
