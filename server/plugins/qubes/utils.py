b80dbde8ea04b93b7c7755970651c62bffd3ed93
M. Drew LaMar <mdlama@wm.edu>
2025-09-22 13:22:16 -0400

    Pull record using QUBES API

--- server/plugins/qubes/utils.py
+++ server/plugins/qubes/utils.py
@@ -8,20 +8,40 @@
 
 
 import httpx
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
+from datetime import datetime
 from server.plugins.ome_plugin import EducationResource
 
-
-def fetch_page(url: str) -> str:
-    return httpx.get(url, follow_redirects=True, timeout=30).raise_for_status().text
-
-
 def extract_from_url(url: str) -> EducationResource:
-    page = fetch_page(url)
-    soup = BeautifulSoup(page, "html.parser")
+    url = f"https://qubeshub.org/oaipmh/?verb=GetRecord&metadataPrefix=qdc&identifier={url}"
+    response = httpx.get(url, follow_redirects=True, timeout=30, headers={"Accept": "application/xml"})
+    response.raise_for_status()
+
+    root = ET.fromstring(response.text)
+    # Define namespaces
+    ns = {
+        "oai": "http://www.openarchives.org/OAI/2.0/",
+        "dc": "http://purl.org/dc/elements/1.1/",
+        "oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/"
+    }
+
+    # Find the metadata section
+    metadata = root.find(".//oai:metadata", ns)
+
+    # Grab Dublin Core elements inside
+    title = metadata.find(".//dc:title", ns).text if metadata.find(".//dc:title", ns) is not None else ""
+    description = metadata.find(".//dc:description", ns).text if metadata.find(".//dc:description", ns) is not None else ""
+    authors = [el.text for el in metadata.findall(".//dc:creator", ns)]
+    subject_tags = [el.text for el in metadata.findall(".//dc:subject", ns)]
+    creation_date = datetime.strptime(metadata.find(".//dc:date", ns).text, "%Y-%m-%dT%H:%M:%SZ") if metadata.find(".//dc:date", ns) is not None else None
+    source_url = metadata.find(".//dc:identifier", ns).text if metadata.find(".//dc:identifier", ns) is not None else ""
+
     return EducationResource(
-        title=soup.select("div.title-wrapper div.title h2")[0].text,
-        description=soup.select("div.abstract-full")[0].text,
-        source_url=url,
+        title=title,
+        description=description,
+        authors=authors,
+        subject_tags=subject_tags,
+        creation_date=creation_date,
+        source_url=source_url,
     )
