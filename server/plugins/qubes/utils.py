f1bc74b04935c94e6ed6cb3f7678effba22a52a9
M. Drew LaMar <mdlama@wm.edu>
2025-09-23 16:50:52 -0400

    Pull record using QUBES API (#203)
    * Pull record using QUBES API
    
    * Fix Python lint issues

--- server/plugins/qubes/utils.py
+++ server/plugins/qubes/utils.py
@@ -1,27 +1,61 @@
 # /// script
 # dependencies = [
-#     "beautifulsoup4",
+#     "xml",
+#     "datetime",
 #     "httpx",
 #     "rich",
 # ]
 # ///
 
+import xml.etree.ElementTree as ET
+from datetime import UTC, datetime
 
 import httpx
-from bs4 import BeautifulSoup
 
 from server.plugins.ome_plugin import EducationResource
 
-
-def fetch_page(url: str) -> str:
-    return httpx.get(url, follow_redirects=True, timeout=30).raise_for_status().text
+ns = {
+    "oai": "http://www.openarchives.org/OAI/2.0/",
+    "dc": "http://purl.org/dc/elements/1.1/",
+    "oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/",
+}
 
 
 def extract_from_url(url: str) -> EducationResource:
-    page = fetch_page(url)
-    soup = BeautifulSoup(page, "html.parser")
+    url = f"https://qubeshub.org/oaipmh/?verb=GetRecord&metadataPrefix=qdc&identifier={url}"
+    response = httpx.get(
+        url, follow_redirects=True, timeout=30, headers={"Accept": "application/xml"}
+    ).raise_for_status()
+    root = ET.fromstring(response.text)  # noqa: S314
+
+    # Find the metadata section
+    metadata = root.find(".//oai:metadata", ns)
+
+    # Grab Dublin Core elements inside
+    title_element = metadata.find(".//dc:title", ns)
+    title = title_element.text if title_element is not None else ""
+
+    description_element = metadata.find(".//dc:description", ns)
+    description = description_element.text if description_element is not None else ""
+
+    authors = [el.text for el in metadata.findall(".//dc:creator", ns)]
+    subject_tags = [el.text for el in metadata.findall(".//dc:subject", ns)]
+
+    if (creation_date_element := metadata.find(".//dc:date", ns)) is not None:
+        creation_date = datetime.strptime(
+            creation_date_element.text, "%Y-%m-%dT%H:%M:%SZ"
+        ).replace(tzinfo=UTC)
+    else:
+        creation_date = None
+
+    source_url_element = metadata.find(".//dc:identifier", ns)
+    source_url = source_url_element.text if source_url_element is not None else ""
+
     return EducationResource(
-        title=soup.select("div.title-wrapper div.title h2")[0].text,
-        description=soup.select("div.abstract-full")[0].text,
-        source_url=url,
+        title=title,
+        description=description,
+        authors=authors,
+        subject_tags=subject_tags,
+        creation_date=creation_date,
+        source_url=source_url,
     )
