4e010ea99eaa88223faa117044e2bba66b2d4f08
M. Drew LaMar <mdlama@wm.edu>
2025-09-23 15:56:02 -0400

    Fix Python lint issues

--- server/plugins/qubes/utils.py
+++ server/plugins/qubes/utils.py
@@ -1,41 +1,55 @@
 # /// script
 # dependencies = [
-#     "beautifulsoup4",
+#     "xml",
+#     "datetime",
 #     "httpx",
 #     "rich",
 # ]
 # ///
 
+import xml.etree.ElementTree as ET
+from datetime import UTC, datetime
 
 import httpx
-import xml.etree.ElementTree as ET
 
-from datetime import datetime
 from server.plugins.ome_plugin import EducationResource
+
+ns = {
+    "oai": "http://www.openarchives.org/OAI/2.0/",
+    "dc": "http://purl.org/dc/elements/1.1/",
+    "oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/",
+}
+
 
 def extract_from_url(url: str) -> EducationResource:
     url = f"https://qubeshub.org/oaipmh/?verb=GetRecord&metadataPrefix=qdc&identifier={url}"
-    response = httpx.get(url, follow_redirects=True, timeout=30, headers={"Accept": "application/xml"})
-    response.raise_for_status()
-
-    root = ET.fromstring(response.text)
-    # Define namespaces
-    ns = {
-        "oai": "http://www.openarchives.org/OAI/2.0/",
-        "dc": "http://purl.org/dc/elements/1.1/",
-        "oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/"
-    }
+    response = httpx.get(
+        url, follow_redirects=True, timeout=30, headers={"Accept": "application/xml"}
+    ).raise_for_status()
+    root = ET.fromstring(response.text)  # noqa: S314
 
     # Find the metadata section
     metadata = root.find(".//oai:metadata", ns)
 
     # Grab Dublin Core elements inside
-    title = metadata.find(".//dc:title", ns).text if metadata.find(".//dc:title", ns) is not None else ""
-    description = metadata.find(".//dc:description", ns).text if metadata.find(".//dc:description", ns) is not None else ""
+    title_element = metadata.find(".//dc:title", ns)
+    title = title_element.text if title_element is not None else ""
+
+    description_element = metadata.find(".//dc:description", ns)
+    description = description_element.text if description_element is not None else ""
+
     authors = [el.text for el in metadata.findall(".//dc:creator", ns)]
     subject_tags = [el.text for el in metadata.findall(".//dc:subject", ns)]
-    creation_date = datetime.strptime(metadata.find(".//dc:date", ns).text, "%Y-%m-%dT%H:%M:%SZ") if metadata.find(".//dc:date", ns) is not None else None
-    source_url = metadata.find(".//dc:identifier", ns).text if metadata.find(".//dc:identifier", ns) is not None else ""
+
+    if (creation_date_element := metadata.find(".//dc:date", ns)) is not None:
+        creation_date = datetime.strptime(
+            creation_date_element.text, "%Y-%m-%dT%H:%M:%SZ"
+        ).replace(tzinfo=UTC)
+    else:
+        creation_date = None
+
+    source_url_element = metadata.find(".//dc:identifier", ns)
+    source_url = source_url_element.text if source_url_element is not None else ""
 
     return EducationResource(
         title=title,
