0f597b3057bebf14fdc6bafb131974790ab50e57
M. Drew LaMar <mdlama@wm.edu>
2025-10-23 14:14:44 -0400

    QUBES now using spdx id (#218)

--- server/plugins/qubes/utils.py
+++ server/plugins/qubes/utils.py
@@ -7,6 +7,7 @@
 
 import xml.etree.ElementTree as ET
 from datetime import UTC, datetime
+from urllib.parse import urlparse
 
 import httpx
 
@@ -19,10 +20,32 @@ ns = {
 }
 
 
+def license_url_to_spdx_id(url: str) -> str:
+    netloc = urlparse(url).netloc
+    path = urlparse(url).path.strip("/").split("/")
+
+    spdx_id = ""
+    if netloc == "creativecommons.org" and len(path) >= 3:
+        version = path[2]  # CC version number
+        if path[0] == "licenses":
+            # Example: http://creativecommons.org/licenses/by-nc-sa/4.0/
+            license_parts = path[1].upper().split("-")  # ['BY', 'NC', 'ND', 'SA']
+            spdx_id = "CC-" + "-".join([*license_parts, version])
+        elif path[0] == "publicdomain" and path[1] == "zero":
+            # Example: http://creativecommons.org/publicdomain/zero/1.0/
+            spdx_id = f"CC0-{version}"
+
+    return spdx_id
+
+
 def extract_from_url(url: str) -> EducationResource:
-    url = f"https://qubeshub.org/oaipmh/?verb=GetRecord&metadataPrefix=qdc&identifier={url}"
+    api_url = f"http://qubeshub.org/oaipmh/?verb=GetRecord&metadataPrefix=qdc&identifier={url}"
+    # api_url = f"http://ddev-qubeshub-web:80/oaipmh/?verb=GetRecord&metadataPrefix=qdc&identifier={url}"
     response = httpx.get(
-        url, follow_redirects=True, timeout=30, headers={"Accept": "application/xml"}
+        api_url,
+        follow_redirects=True,
+        timeout=30,
+        headers={"Accept": "application/xml"},
     ).raise_for_status()
     root = ET.fromstring(response.text)  # noqa: S314
 
@@ -46,8 +69,17 @@ def extract_from_url(url: str) -> EducationResource:
     else:
         creation_date = None
 
-    source_url_element = metadata.find(".//dc:identifier", ns)
-    source_url = source_url_element.text if source_url_element is not None else ""
+    version_url_element = metadata.find(".//dc:identifier", ns)
+    version_url = version_url_element.text if version_url_element is not None else ""
+
+    source_url = "/".join([*url.rstrip("/").split("/")[:-1], "main"])
+
+    spdx_license_expression_element = metadata.find(".//dc:rights", ns)
+    spdx_license_expression = (
+        license_url_to_spdx_id(spdx_license_expression_element.text)
+        if spdx_license_expression_element is not None
+        else ""
+    )
 
     return EducationResource(
         title=title,
@@ -56,4 +88,6 @@ def extract_from_url(url: str) -> EducationResource:
         subject_tags=subject_tags,
         creation_date=creation_date,
         source_url=source_url,
+        version_url=version_url,
+        spdx_license_expression=spdx_license_expression,
     )
