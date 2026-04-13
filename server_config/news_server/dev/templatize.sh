fa639fc3ead2157eff24b500d1466514b88f2eb6
anooparyal <anoop.aryal@gmail.com>
2026-04-13 01:04:10 -0500

    last of the config files for NNTP is templatized.

--- server_config/news_server/dev/templatize.sh
+++ server_config/news_server/dev/templatize.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-DIR=$1
-CONFIG=$2
+FILTERS=$1
+DIR=$2
+CONFIG=$3
 
-find ${DIR} -name '*.j2' | while read f; do j2 -o `dirname ${f}`/`basename ${f} .j2` ${f} ${CONFIG} ; done
+find ${DIR} -name '*.j2' | while read f; do j2 --filters=${FILTERS} -o `dirname ${f}`/`basename ${f} .j2` ${f} ${CONFIG} ; done
