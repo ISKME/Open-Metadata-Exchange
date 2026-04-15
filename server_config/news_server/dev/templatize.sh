8665eca98ea968b3786c6f366d07b1a40d94e812
anooparyal <anoop.aryal@gmail.com>
2026-04-14 21:54:19 -0500

    had to turn a one liner into a script to quote properly.

--- server_config/news_server/dev/templatize.sh
+++ server_config/news_server/dev/templatize.sh
@@ -6,4 +6,10 @@ FILTERS=$1
 DIR=$2
 CONFIG=$3
 
-find "${DIR}" -name '*.j2' | while read -r f; do j2 --filters=${FILTERS} -o $(dirname "${f}")/$(basename "${f}" .j2) "${f}" "${CONFIG}" ; done
+find "${DIR}" -name '*.j2' | while read -r f;
+do
+    DirName=$(dirname "${f}")
+    BaseName=$(basename "${f}" .j2)
+    FN="${DirName}/${BaseName}"
+    j2 --filters=${FILTERS} -o "${FN}" "${f}" "${CONFIG}";
+done
