342cd28fe5365add150de84fe9c99dce2d92454b
Christian Clauss <cclauss@me.com>
2026-04-27 17:01:13 +0545

    return (await httpx_async_client.get(url)).check_for_status().text

--- demo/michigan_mashup.py
+++ demo/michigan_mashup.py
@@ -50,7 +50,7 @@ def getty(page: str) -> str:
 async def fetch_text(url: str) -> str:
     async with httpx.AsyncClient() as httpx_async_client:
         try:
-            response = await httpx_async_client.get(url).check_for_status()
+            return (await httpx_async_client.get(url)).check_for_status().text
         except httpx.HTTPStatusError:
             print(f"Failed to fetch {url}: {response.status_code}")
             raise
