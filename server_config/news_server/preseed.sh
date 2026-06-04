171b2e1f12ea34fc23e5faa6f63d4d30d6757df0
anooparyal <anoop.aryal@gmail.com>
2026-06-03 23:31:02 -0500

    adding 'port' to the config for peers. templatizing the seeding script.

--- server_config/news_server/preseed.sh
+++ server_config/news_server/preseed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-/usr/lib/news/bin/actsync -v 4 -m -ox -p 20  localhost nntp.dev.ome.oercommons.org
+/usr/lib/news/bin/actsync -v 4 -m -ox -p 0  localhost nntp.dev.ome.oercommons.org
 /usr/lib/news/bin/getlist -h localhost | \
     sed -n 's/^\(ome\.[^ ]*\) .*/\t\1/pg' | \
     awk 'BEGIN{print("nntp.dev.ome.oercommons.org");}{print($0);}' > /root/pullnews.marks
